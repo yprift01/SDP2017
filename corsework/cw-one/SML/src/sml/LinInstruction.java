deb2c8e3fd1081790514f8138a34306d1324e841
Ylli Prifti <yprift01@mail.bbk.ac.uk>
2017-02-04 17:13:24 +0000

    first push cw-one

---
+++ corsework/cw-one/SML/src/sml/LinInstruction.java
@@ -0,0 +1,27 @@
+package sml;
+
+public class LinInstruction extends Instruction {
+    private int register;
+    private int value;
+
+    public LinInstruction(String label, String opcode) {
+        super(label, opcode);
+    }
+
+    public LinInstruction(String label, int register, int value) {
+        super(label, "lin");
+        this.register = register;
+        this.value = value;
+
+    }
+
+    @Override
+    public void execute(Machine m) {
+        m.getRegisters().setRegister(register, value);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " register " + register + " value is " + value;
+    }
+}
